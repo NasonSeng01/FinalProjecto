d7fd1c8293fc569564d95e004173d4819f9477e4
NasonSeng01 <nasonseng23@gmail.com>
2019-07-23 21:04:36 -0400

    heroku

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Nav from './components/Nav';
+import SBCards from './components/sbCards';
+import SYCards from './components/syCards';
 
 function App() {
  
   return ( 
     <div className="App">
-      <p>Hello worlds</p>
+      <Nav>
+      </Nav>
+      <SBCards></SBCards>
+        <SYCards></SYCards>
     </div>
   );
 
